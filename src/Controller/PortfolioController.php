1eda299e100ad230a92c61aeca54378cc8fab237
Marbobley <fournier.wilf@gmail.com>
2026-01-13 22:42:43 +0100

    add portfolio

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -11,8 +11,17 @@ final class PortfolioController extends AbstractController
     #[Route('/portfolio', name: 'app_portfolio')]
     public function index(): Response
     {
+        $images = [
+            ['src' => 'dosCeci.webp', 'alt' => 'Tatouage dos floral et dentelle'],
+            ['src' => 'dosDamien.webp', 'alt' => 'Tatouage dos complet'],
+            ['src' => 'piedTulipe.webp', 'alt' => 'Tatouage tulipe sur le pied'],
+            ['src' => 'meduse.webp', 'alt' => 'Tatouage méduse'],
+            ['src' => 'ninja.webp', 'alt' => 'Tatouage inspiration ninja'],
+            ['src' => 'rosePied.webp', 'alt' => 'Tatouage rose sur le pied'],
+        ];
+
         return $this->render('portfolio/index.html.twig', [
-            'controller_name' => 'PortfolioController',
+            'images' => $images,
         ]);
     }
 }
